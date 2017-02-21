0c21bfebc556720dd3f94ec9b8fd6d478486dbae
Ryan McCue <me@ryanmccue.info>
2017-02-21 16:50:37 +1000

    Add plugin header

--- plugin.php
+++ plugin.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Plugin Name: Standard Chartered Replace Files
+ * Description: Allow replacing files after they've been uploaded.
+ * Author: Human Made
+ * Author URI: https://hmn.md/
+ */
 
 namespace SC\Replace_Files;
 
